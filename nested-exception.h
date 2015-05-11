7d78cb591f3bba6b82a4d4305f375b343804609b
Pete Cordell <pete.git@codalogic.com>
2015-05-11 20:58:03 +0100

    Initial commit

---
+++ nested-exception.h
@@ -0,0 +1,195 @@
+//----------------------------------------------------------------------------
+// Copyright (c) 2015, Codalogic Ltd (http://www.codalogic.com)
+// All rights reserved.
+//
+// The license for this file is based on the BSD-3-Clause license
+// (http://www.opensource.org/licenses/BSD-3-Clause).
+//
+// Redistribution and use in source and binary forms, with or without 
+// modification, are permitted provided that the following conditions 
+// are met:
+//
+// - Redistributions of source code must retain the above copyright notice, 
+//   this list of conditions and the following disclaimer.
+// - Redistributions in binary form must reproduce the above copyright notice,
+//   this list of conditions and the following disclaimer in the documentation
+//   and/or other materials provided with the distribution.
+// - Neither the name Codalogic Ltd nor the names of its contributors may be
+//   used to endorse or promote products derived from this software without 
+//   specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// POSSIBILITY OF SUCH DAMAGE.
+//----------------------------------------------------------------------------
+
+#ifndef NESTED_EXCEPTION
+#define NESTED_EXCEPTION
+
+#include <exception>
+#include <string>
+#include <vector>
+#include <list>
+#include <ostream>
+#include <sstream>
+
+namespace nest_exp {
+
+struct NestedExceptionParameter
+{
+	const char * name;
+	std::string value;
+
+	NestedExceptionParameter( 
+			const char * const name_in,
+			std::string value_in )
+		:
+		name( name_in ),
+		value( value_in )
+	{}
+};
+
+class NestedExceptionParams
+{
+private:
+	std::vector< NestedExceptionParameter > params;
+
+public:
+	NestedExceptionParams() {}
+	NestedExceptionParams( 
+			const char * const name_in,
+			const std::string & value_in )
+	{
+		add( name_in, value_in );
+	}
+	template< typename T >
+	NestedExceptionParams( 
+			const char * const name_in,
+			const T & value_in )
+	{
+		add( name_in, value_in );
+	}
+
+	void add( 
+			const char * const name_in,
+			const std::string & value_in )
+	{
+		params.push_back( NestedExceptionParameter( name_in, value_in ) );
+	}
+	template< typename T >
+	void add( 
+			const char * const name_in,
+			const T & value_in )
+	{
+		std::stringstream ss;
+		ss << value_in;
+		params.push_back( NestedExceptionParameter( name_in, ss.str() ) );
+	}
+
+	bool empty() const { return params.empty(); }
+	size_t size() const { return params.size(); }
+	const NestedExceptionParameter & operator []( size_t i ) const { return params[i]; }
+	NestedExceptionParameter & operator []( size_t i ) { return params[i]; }
+};
+
+struct NestedExceptionNode
+{
+	const char * const error_uri;	// of the form "com.codalogic.mymodule.myerror" or ".mymodule.myerror"
+	NestedExceptionParams error_params;
+	const char * const description;	// Human readable description
+	
+	NestedExceptionNode(
+			const char * const error_uri_in,
+			NestedExceptionParams & error_params_in,
+			const char * const description_in )
+		:
+		error_uri( error_uri_in ),
+		error_params( error_params_in ),
+		description( description_in )
+	{
+	}
+	friend std::ostream & operator << ( std::ostream & os, const NestedExceptionNode & r_node )
+	{
+		os << r_node.error_uri;
+		if( ! r_node.error_params.empty() )
+		{
+			os << " (";
+			os << ")";
+		}
+		os << ": " << r_node.description;
+		return os;
+	}
+};
+
+class NestedException : public std::exception
+{
+private:
+	typedef std::list< NestedExceptionNode > nodes_t;
+	mutable nodes_t nodes;
+
+public:
+	NestedException(
+			const char * const error_uri_in,
+			NestedExceptionParams & error_params_in,
+			const char * const description_in )
+	{
+		nodes.push_front( NestedExceptionNode( error_uri_in, error_params_in, description_in ) );
+	}
+	NestedException(
+			const char * const error_uri_in,
+			NestedExceptionParams & error_params_in,
+			const char * const description_in,
+			NestedException & r_prev_nested_exception )
+	{
+		nodes.swap( r_prev_nested_exception.nodes );
+		nodes.push_front( NestedExceptionNode( error_uri_in, error_params_in, description_in ) );
+	}
+    virtual const char * what() const
+    {
+		if( ! nodes.empty() )
+			return nodes.front().description;
+		return "<Undescribed NestedException>";
+    }
+    
+    template< typename TinspectorFunctor >
+    void inspect( TinspectorFunctor & inspector ) const
+    {
+		for( nodes_t::const_iterator i( nodes.begin() ),
+					i_end( nodes.ends() );
+				i != i_end;
+				++i )
+			inspector( *i );
+    }
+    bool empty() const { return nodes.empty(); }
+    
+    virtual ~NestedException() {}
+};
+
+class NestedExceptionPrinter
+{
+private:
+	size_t indent;
+	std::ostream & os;
+
+public:
+	static void print( std::ostream & os_in, const NestedException & nested_exception_in )
+	{
+	}
+	
+	void operator()( const NestedExceptionNode & r_node )
+	{
+		os << std::string( 2 * indent, ' ' ) << r_node;
+	}
+};
+
+}	// Namespace namespace nest_exp
+
+#endif	// NESTED_EXCEPTION
