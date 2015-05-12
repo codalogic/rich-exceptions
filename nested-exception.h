624ac549cf1e3048f64ffa086449261efabc6621
Pete Cordell <pete.git@codalogic.com>
2015-05-12 14:25:04 +0100

    Completed fleshing out how I want the class to look

--- nested-exception.h
+++ nested-exception.h
@@ -5,17 +5,17 @@
 // The license for this file is based on the BSD-3-Clause license
 // (http://www.opensource.org/licenses/BSD-3-Clause).
 //
-// Redistribution and use in source and binary forms, with or without 
-// modification, are permitted provided that the following conditions 
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions
 // are met:
 //
-// - Redistributions of source code must retain the above copyright notice, 
+// - Redistributions of source code must retain the above copyright notice,
 //   this list of conditions and the following disclaimer.
 // - Redistributions in binary form must reproduce the above copyright notice,
 //   this list of conditions and the following disclaimer in the documentation
 //   and/or other materials provided with the distribution.
 // - Neither the name Codalogic Ltd nor the names of its contributors may be
-//   used to endorse or promote products derived from this software without 
+//   used to endorse or promote products derived from this software without
 //   specific prior written permission.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
@@ -23,11 +23,11 @@
 // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
 // ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
 // LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
 // SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
 // INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
 // CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 // POSSIBILITY OF SUCH DAMAGE.
 //----------------------------------------------------------------------------
 
@@ -41,178 +41,207 @@
 #include <ostream>
 #include <sstream>
 
-namespace nest_exp {
+namespace nest_excep {
 
 struct NestedExceptionParameter
 {
-	const char * name;
-	std::string value;
+    const char * name;
+    std::string value;
 
-	NestedExceptionParameter( 
-			const char * const name_in,
-			std::string value_in )
-		:
-		name( name_in ),
-		value( value_in )
-	{}
-	friend std::ostream & operator << ( std::ostream & os, const NestedExceptionParameter & r_params )
-	{
-		os << r_params.name << ": " << r_params.value;
-		return os;
-	}
+    NestedExceptionParameter(
+            const char * const name_in,
+            std::string value_in )
+        :
+        name( name_in ),
+        value( value_in )
+    {}
+    friend std::ostream & operator << ( std::ostream & os, const NestedExceptionParameter & r_params )
+    {
+        os << r_params.name << ": " << r_params.value;
+        return os;
+    }
 };
 
 class NestedExceptionParams
 {
 private:
-	typedef std::vector< NestedExceptionParameter > params_t;
-	params_t params;
+    typedef std::vector< NestedExceptionParameter > params_t;
+    params_t params;
 
 public:
-	NestedExceptionParams() {}
-	NestedExceptionParams( 
-			const char * const name_in,
-			const std::string & value_in )
-	{
-		add( name_in, value_in );
-	}
-	template< typename T >
-	NestedExceptionParams( 
-			const char * const name_in,
-			const T & value_in )
-	{
-		add( name_in, value_in );
-	}
+    NestedExceptionParams() {}
+    NestedExceptionParams(
+            const char * const name_in,
+            const std::string & value_in )
+    {
+        add( name_in, value_in );
+    }
+    template< typename T >
+    NestedExceptionParams(
+            const char * const name_in,
+            const T & value_in )
+    {
+        add( name_in, value_in );
+    }
 
-	void add( 
-			const char * const name_in,
-			const std::string & value_in )
-	{
-		params.push_back( NestedExceptionParameter( name_in, value_in ) );
-	}
-	template< typename T >
-	void add( 
-			const char * const name_in,
-			const T & value_in )
-	{
-		std::stringstream ss;
-		ss << value_in;
-		params.push_back( NestedExceptionParameter( name_in, ss.str() ) );
-	}
+    void add(
+            const char * const name_in,
+            const std::string & value_in )
+    {
+        params.push_back( NestedExceptionParameter( name_in, value_in ) );
+    }
+    template< typename T >
+    void add(
+            const char * const name_in,
+            const T & value_in )
+    {
+        std::stringstream ss;
+        ss << value_in;
+        params.push_back( NestedExceptionParameter( name_in, ss.str() ) );
+    }
 
-	bool empty() const { return params.empty(); }
-	size_t size() const { return params.size(); }
-	const NestedExceptionParameter & operator []( size_t i ) const { return params[i]; }
-	NestedExceptionParameter & operator []( size_t i ) { return params[i]; }
+    bool empty() const { return params.empty(); }
+    size_t size() const { return params.size(); }
+    const NestedExceptionParameter & operator []( size_t i ) const { return params[i]; }
+    NestedExceptionParameter & operator []( size_t i ) { return params[i]; }
 
-	friend std::ostream & operator << ( std::ostream & os, const NestedExceptionParams & r_params )
-	{
-		bool is_first = true;
-		for( params_t::const_iterator i( r_params.params.begin() ),
-					i_end( r_params.params.end() );
-				i != i_end;
-				++i )
-		{
-			if( ! is_first )
-				os << ", ";
-			os << *i;
-		}
-		return os;
-	}
+    friend std::ostream & operator << ( std::ostream & os, const NestedExceptionParams & r_params )
+    {
+        bool is_first = true;
+        for( params_t::const_iterator i( r_params.params.begin() ),
+                    i_end( r_params.params.end() );
+                i != i_end;
+                ++i )
+        {
+            if( ! is_first )
+                os << ", ";
+            os << *i;
+        }
+        return os;
+    }
 };
 
 struct NestedExceptionNode
 {
-	const char * const error_uri;	// of the form "com.codalogic.mymodule.myerror" or ".mymodule.myerror"
-	NestedExceptionParams error_params;
-	const char * const description;	// Human readable description
-	
-	NestedExceptionNode(
-			const char * const error_uri_in,
-			NestedExceptionParams & error_params_in,
-			const char * const description_in )
-		:
-		error_uri( error_uri_in ),
-		error_params( error_params_in ),
-		description( description_in )
-	{
-	}
+    const char * const error_uri;   // of the form "com.codalogic.mymodule.myerror" or ".mymodule.myerror"
+    NestedExceptionParams error_params;
+    const char * const description; // Human readable description
 
-	friend std::ostream & operator << ( std::ostream & os, const NestedExceptionNode & r_node )
-	{
-		os << r_node.error_uri;
-		if( ! r_node.error_params.empty() )
-			os << " (" << r_node.error_params << ")";
-		os << ": " << r_node.description;
-		return os;
-	}
+    NestedExceptionNode(
+            const char * const error_uri_in,
+            const char * const description_in )
+        :
+        error_uri( error_uri_in ),
+        description( description_in )
+    {
+    }
+    NestedExceptionNode(
+            const char * const error_uri_in,
+            NestedExceptionParams & error_params_in,
+            const char * const description_in )
+        :
+        error_uri( error_uri_in ),
+        error_params( error_params_in ),
+        description( description_in )
+    {
+    }
+
+    friend std::ostream & operator << ( std::ostream & os, const NestedExceptionNode & r_node )
+    {
+        os << r_node.error_uri;
+        if( ! r_node.error_params.empty() )
+            os << " (" << r_node.error_params << ")";
+        os << ": " << r_node.description;
+        return os;
+    }
 };
 
 class NestedException : public std::exception
 {
 private:
-	typedef std::list< NestedExceptionNode > nodes_t;
-	mutable nodes_t nodes;
+    typedef std::list< NestedExceptionNode > nodes_t;
+    mutable nodes_t nodes;
 
 public:
-	NestedException(
-			const char * const error_uri_in,
-			NestedExceptionParams & error_params_in,
-			const char * const description_in )
-	{
-		nodes.push_front( NestedExceptionNode( error_uri_in, error_params_in, description_in ) );
-	}
-	NestedException(
-			const char * const error_uri_in,
-			NestedExceptionParams & error_params_in,
-			const char * const description_in,
-			NestedException & r_prev_nested_exception )
-	{
-		nodes.swap( r_prev_nested_exception.nodes );
-		nodes.push_front( NestedExceptionNode( error_uri_in, error_params_in, description_in ) );
-	}
+    NestedException(
+            const char * const error_uri_in,
+            const char * const description_in )
+    {
+        nodes.push_front( NestedExceptionNode( error_uri_in, description_in ) );
+    }
+    NestedException(
+            const char * const error_uri_in,
+            NestedExceptionParams & error_params_in,
+            const char * const description_in )
+    {
+        nodes.push_front( NestedExceptionNode( error_uri_in, error_params_in, description_in ) );
+    }
+    NestedException(
+            const char * const error_uri_in,
+            const char * const description_in,
+            NestedException & r_prev_nested_exception )
+    {
+        nodes.swap( r_prev_nested_exception.nodes );
+        nodes.push_front( NestedExceptionNode( error_uri_in, description_in ) );
+    }
+    NestedException(
+            const char * const error_uri_in,
+            NestedExceptionParams & error_params_in,
+            const char * const description_in,
+            NestedException & r_prev_nested_exception )
+    {
+        nodes.swap( r_prev_nested_exception.nodes );
+        nodes.push_front( NestedExceptionNode( error_uri_in, error_params_in, description_in ) );
+    }
     virtual ~NestedException() {}
 
     virtual const char * what() const
     {
-		if( ! nodes.empty() )
-			return nodes.front().description;
-		return "<Undescribed NestedException>";
+        if( ! nodes.empty() )
+            return nodes.front().description;
+        return "<Undescribed NestedException>";
     }
-    
+
     template< typename TinspectorFunctor >
     void inspect( TinspectorFunctor & inspector ) const
     {
-		for( nodes_t::const_iterator i( nodes.begin() ),
-					i_end( nodes.end() );
-				i != i_end;
-				++i )
-			inspector( *i );
+        for( nodes_t::const_iterator i( nodes.begin() ), i_end( nodes.end() );
+                i != i_end;
+                ++i )
+            inspector( *i );
+    }
+    template< typename TinspectorFunctor >
+    void reverse_inspect( TinspectorFunctor & inspector ) const
+    {
+        for( nodes_t::const_reverse_iterator i( nodes.rbegin() ), i_end( nodes.rend() );
+                i != i_end;
+                ++i )
+            inspector( *i );
     }
     bool empty() const { return nodes.empty(); }
 
-	class NestedExceptionPrinter
-	{
-	private:
-		size_t indent;
-		std::ostream & os;
+    class NestedExceptionPrinter
+    {
+    private:
+        size_t indent;
+        std::ostream & os;
 
-	public:
-		NestedExceptionPrinter( std::ostream & os_in ) : indent( 0 ), os( os_in )
-		{}
-		void operator()( const NestedExceptionNode & r_node )
-		{
-			os << std::string( 2 * indent, ' ' ) << r_node;
-		}
-	};
+    public:
+        NestedExceptionPrinter( std::ostream & os_in ) : indent( 0 ), os( os_in )
+        {}
+        void operator()( const NestedExceptionNode & r_node )
+        {
+            os << std::string( 2 * indent, ' ' ) << r_node;
+        }
+    };
 
-	friend std::ostream & operator << ( std::ostream & os, const NestedException & r_exception )
-	{
-		NestedExceptionPrinter nested_exception_pointer( os );
-		r_exception.inspect( nested_exception_pointer );
-	}
+    friend std::ostream & operator << ( std::ostream & os, const NestedException & r_exception )
+    {
+        NestedExceptionPrinter nested_exception_pointer( os );
+        r_exception.inspect( nested_exception_pointer );
+    }
 };
 
-}	// Namespace namespace nest_exp
+}   // Namespace namespace nest_excep
 
-#endif	// NESTED_EXCEPTION
+#endif  // NESTED_EXCEPTION
